0e8dedad23d162a2560f7a9374fcd38fd6d7510a
Parker Jones <74691370+ParkerHJones@users.noreply.github.com>
2021-09-03 05:18:35 -0600

    controllers for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,11 +5,39 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
+    render component: 'Artist', props: { artist: @artist }
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+    else
+      render component: 'ArtistNew', props: { artist: @artist}
+    end 
+  end 
 
   def new
+    @artist = Artist.new
+    render component: 'ArtistNew', props { artist: @artist }
   end
 
+  def update 
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+    else 
+      render component: 'ArtistEdit', props: { artist: @artist}
+    end 
+  end 
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    Artist.find(params[:id])destroy
+  end 
+  
   def edit
+    @artist = Artist.find(params[:id])
+    render component: 'ArtistEdit', props: { artist: @artist}
   end
 end
